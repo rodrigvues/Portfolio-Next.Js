aa53cfa1f6f8706d444561b5c79d431393f57c2a
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-19 22:11:05 -0300

    fixing projetos styles / fixing project images

--- components/projetos.tsx
+++ components/projetos.tsx
@@ -23,8 +23,8 @@ export default function Projetos() {
 type ProjetoProps = typeof projectsData[number];
 
 function Projeto({ title, description, tags, imageUrl }: ProjetoProps) {
-    return  <section className='group bg-gray-100 mas-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition'>
-                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
+    return  <section className='group bg-gray-100 mas-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition'>
+                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
                     <h3 className='text-2xl font-semibold'>{title}</h3>
                     <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
                     <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -39,7 +39,14 @@ function Projeto({ title, description, tags, imageUrl }: ProjetoProps) {
                     src={imageUrl} 
                     alt="Meu Projeto" 
                     quality={95} 
-                    className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40'
+                    className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+                    transition
+                    group-hover:scale-[1.04]
+                    group-hover:-translate-x-3
+                    group-hover:translate-y-3
+                    group-hover:-rotate-2
+                    group-even:right-[initial] 
+                    group-even:-left-40'
                 />
             </section>
 }
