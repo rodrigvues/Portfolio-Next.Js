9a21407bf3858ddee46815e2f920c4b810ec4383
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-19 22:59:19 -0300

    projects animation

--- components/projetos.tsx
+++ components/projetos.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import React from 'react'
+import React, { useRef } from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
-import Image from 'next/image'
+import Projeto from './projeto'
 
 export default function Projetos() {
   return (
@@ -18,40 +18,4 @@ export default function Projetos() {
         </div>
     </section>
   )
-}
-
-type ProjetoProps = typeof projectsData[number];
-
-function Projeto({ title, description, tags, imageUrl }: ProjetoProps) {
-    return  <section className='group bg-gray-100 mas-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition'>
-                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
-                    <h3 className='text-2xl font-semibold'>{title}</h3>
-                    <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
-                    <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
-                        {tags.map((tag, index) => (
-                            <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
-                        ))}
-                    </ul>
-                </div>
-                
-
-                <Image 
-                    src={imageUrl} 
-                    alt="Meu Projeto" 
-                    quality={95} 
-                    className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
-                    transition
-                    group-hover:scale-[1.04]
-                    group-hover:-translate-x-3
-                    group-hover:translate-y-3
-                    group-hover:-rotate-2
-
-                    group-even:group-hover:translate-x-3
-                    group-even:group-hover:translate-y-3
-                    group-even:group-hover:rotate-2
-
-                    group-even:right-[initial] 
-                    group-even:-left-40'
-                />
-            </section>
 }
