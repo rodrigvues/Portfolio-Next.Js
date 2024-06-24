a7b7c40f8ef9a61acff59a16469230714b1ac549
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-24 19:28:10 -0300

    borderblack in everything

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,19 +92,19 @@ export default function Intro() {
           </Link>
 
 
-          <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10'
+          <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer borderBlack'
           href='/CV.pdf'download
           >
             Baixe meu Currículo <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/> 
           </a>
 
 
-          <a href="https://linkedin.com/in/rodrigvues" target="_blank" className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105 transition cursor-pointer border border-black/10'>
+          <a href="https://linkedin.com/in/rodrigvues" target="_blank" className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105 transition cursor-pointer borderBlack'>
             <BsLinkedin />
           </a>
 
 
-          <a href="https://github.com/rodrigvues" target="_blank" className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'>
+          <a href="https://github.com/rodrigvues" target="_blank" className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack'>
             <FaGithubSquare />
           </a>
         </motion.div>
