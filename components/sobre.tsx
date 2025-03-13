387d6b86a6f73d2b2fe9d13d57f3dced29984182
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2025-03-12 23:51:02 -0300

    updating

--- components/sobre.tsx
+++ components/sobre.tsx
@@ -22,36 +22,42 @@ export default function Sobre() {
     >
         <SectionHeading>Sobre mim</SectionHeading>
         <p className='mb-3'>
-  Desde criança, já pensava em começar uma carreira na área de{" "}
-  <span className="font-medium">programação</span> pelo meu interesse na{" "}
-  <span className="font-medium">internet</span> e em computadores, e logo{" "}
-  <span className="font-medium">comecei</span> o curso técnico em desenvolvimento de{" "}
-  <span className="font-medium">sistemas</span>, onde aprendi como realmente{" "}
-  <span className="font-medium">programar na prática</span>. Aprendi principalmente{" "}
-  <span className="font-medium">lógica, programação orientada a objetos</span> e{" "}
-  <span className="font-medium">banco de dados</span>, com{" "}
-  <span className="font-medium">Java e MySQL</span>. No final de 2023, tive também a{" "}
-  <span className="font-medium">oportunidade</span> de participar de um{" "}
-  <span className="font-medium">Bootcamp de Next.js</span> na{" "}
-  <span className="font-medium">Escola Eliti</span>, onde por pouco mais de{" "}
-  <span className="font-medium">dois meses</span> e dentro de um ambiente{" "}
-  <span className="font-medium">profissional</span>, na sede da{" "}
-  <span className="font-medium">AMcom</span> (que também me preparou muito para o{" "}
-  <span className="font-medium">começo na área</span>), me aprofundei no{" "}
-  <span className="font-medium">front-end</span> criando meus primeiros projetos em{" "}
-  <span className="font-medium">Next.js</span>. No Bootcamp,{" "}
-  <span className="font-medium">aprendi muito TypeScript e React</span>, e foi ali{" "}
-  que fui levado para o lado do{" "}
-  <span className="font-medium">Front-end</span>, meu principal foco no momento.
+  Desde pequeno, sempre fui fascinado por{" "}
+  <span className="font-medium">tecnologia, internet</span> e{" "}
+  <span className="font-medium">computadores</span>. Esse interesse me levou a{" "}
+  <span className="font-medium">cursar Desenvolvimento de Sistemas</span> no{" "}
+  <span className="font-medium">SENAI</span> em <span className="font-medium">2022</span>, 
+  onde aprendi <span className="font-medium">programação na prática</span>, com foco em{" "}
+  <span className="font-medium">lógica, Programação Orientada a Objetos</span> e{" "}
+  <span className="font-medium">bancos de dados</span>, utilizando{" "}
+  <span className="font-medium">Java e MySQL</span>.
+</p>
+<p className='mb-3'>
+  Ao longo da minha formação, conheci{" "}
+  <span className="font-medium">React e Next.js</span> e coloquei meu foco no{" "}
+  <span className="font-medium">Front-end</span>. Já em{" "}
+  <span className="font-medium">2024</span>, ainda antes de me formar em{" "}
+  <span className="font-medium">Desenvolvimento de Sistemas</span>, entrei no{" "}
+  <span className="font-medium">programa +Devs2Blu</span> da{" "}
+  <span className="font-medium">Blusoft</span>, onde estou me aprofundando no{" "}
+  <span className="font-medium">desenvolvimento mobile</span> com{" "}
+  <span className="font-medium">Flutter</span>. Esse curso tem sido um{" "}
+  <span className="font-medium">divisor de águas</span> na minha jornada, pois me{" "}
+  <span className="font-medium">permitiu explorar</span> a criação de{" "}
+  <span className="font-medium">aplicativos do zero</span>, entender melhor a{" "}
+  <span className="font-medium">estrutura de um projeto mobile</span> e aprimorar{" "}
+  minhas habilidades com <span className="font-medium">Dart</span> e{" "}
+  <span className="font-medium">bancos de dados integrados</span>.
 </p>
 <p>
-  <span className="italic">Fora da programação</span>, gosto muito de{" "}
-  <span className="font-medium">esportes no geral</span>, principalmente{" "}
-  <span className="font-medium">futebol</span>, onde acompanho as ligas{" "}
-  <span className="font-medium">brasileiras e inglesas</span>. Também adoro{" "}
-  <span className="font-medium">andar de bike</span>, viajar e conhecer{" "}
-  <span className="font-medium">lugares novos</span> no geral.
+  <span className="italic">Fora da programação</span>, sou apaixonado por{" "}
+  <span className="font-medium">esportes</span>, especialmente{" "}
+  <span className="font-medium">futebol</span>, acompanhando tanto o cenário{" "}
+  <span className="font-medium">brasileiro quanto o europeu</span>. Também gosto{" "}
+  de <span className="font-medium">pedalar</span>, viajar e{" "}
+  <span className="font-medium">explorar novos lugares</span>.
 </p>
+
 
     </motion.section>
   )
