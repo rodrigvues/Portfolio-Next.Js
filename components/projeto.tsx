39c7c676fd56dcc6ad2c73d574aebc431030eb6c
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2025-04-24 22:39:24 -0300

    border

--- components/projeto.tsx
+++ components/projeto.tsx
@@ -45,7 +45,7 @@ export default function Projeto({ title, description, tags, imageUrl, link }: Pr
                         src={imageUrl} 
                         alt="Meu Projeto" 
                         quality={95} 
-                        className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+                        className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-lg shadow-2xl
                         transition 
                         group-hover:scale-[1.04]
                         group-hover:-translate-x-3
